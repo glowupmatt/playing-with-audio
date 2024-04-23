9f47dea5cfec93ea2be8c61120608b3374f0d43b
Glowupmatt <glowupmatt@gmail.com>
2024-04-22 19:52:10 -0700

    add dependencies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,14 @@
+"use client";
+import React, { useRef, useEffect } from "react";
 import Image from "next/image";
 import { Howl, Howler } from "howler";
+import WaveSurfer, { WaveSurferOptions } from "wavesurfer.js";
 
 export default function Home() {
-  var sound = new Howl({
-    src: ['sound.mp3']
-  });
-  
-  sound.play();
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold">Hello, World!</h1>
-      <Image
-        src="/images/nextjs.svg"
-        alt="Next.js logo"
-        width={300}
-        height={300}
-      />
-    </main>
+    <div>
+      <h1>Audio Visualizer</h1>
+      <div />
+    </div>
   );
 }
